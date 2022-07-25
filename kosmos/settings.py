a9d843a3b81076c86a9c44376b9876017dd46bfd
admin <kuligturan@mail.ri>
2022-07-25 22:48:22 +0600

    fix

--- kosmos/settings.py
+++ kosmos/settings.py
@@ -129,14 +129,6 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'main.User'
 
-JWT_AUTH = {
-
-    'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=3000),
-    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-
-}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
